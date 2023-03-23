1f94ff097986f31e30e4d3f9beeddff87e9df7d2
t.inoue <takayuki.inoue@i-studio.co.jp>
2023-03-23 19:52:42 +0900

    change

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 import {zeroPadding} from "./modules/zeroPadding";
 
 // Strings
-String.prototype.zeroPadding = function (length: number) {
+String.prototype.zeroPadding = function (length: number): string{
   return zeroPadding(this as string, length);
 };
 
 // Numbers
-Number.prototype.zeroPadding= function (length: number) {
+Number.prototype.zeroPadding= function (length: number):string {
   return zeroPadding(this as number, length);
 };
 
 // Array
-Array.prototype.deleteDuplicate = function () {
+Array.prototype.deleteDuplicate = function <T>(): T[] {
   const set = new Set(this);
   return [...set];
 };
+
